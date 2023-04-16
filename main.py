64faa7c279d76f988ac5351f13199cad29dde32e
equationslayer12 <roy0708usa@gmail.com>
2023-04-16 23:00:52 +0300

    ACTUALLY STARTING THE PROJECT LMAO

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
-print("Hello, sucker.")
-print("you suck ass bro")
-a = 58
-game_run = True
+import discord
+from discord.ext import commands
+
+bot = commands.Bot("")
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+
+@bot.command()
+async def stop(ctx):
+    ...
 
 
-def deez():
-    print("nuts")
-
-
-while game_run == True or game_run and game_run == True:
-    pass
-
-# f3 e6 g4 qh4#
-
-
-deez()
+bot.run("")  # TODO: add token
