f6bc5de30f95b3c7279c781aeb75a37aa8b7daba
iKappyyyy <specialtimewithdaniel@gmail.com>
2023-04-16 23:58:37 +0300

    - Added file that contains the Default bot username and Default bot PFP

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import discord
 from discord.ext import commands
+from os import environ
 
-bot = commands.Bot(command_prefix="!!", intents=discord.Intents.all())
+bot = commands.Bot(command_prefix="!!", intents=discord.Intents.all(), activity=discord.Game("In The Hunger Games"))
+TOKEN = environ.get('DISCORDO_BOTTO_TOKEN')
 
 
 @bot.event
@@ -9,10 +11,9 @@ async def on_ready():
     print(f'{bot.user} has connected to Discord!')
 
 
-@bot.command()
+@bot.command(name="tempalato")
 async def tempalato(ctx):
     ...
 
 
-bot.run("MTA5NzE5OTc2NDIyMDY3NDA2OQ.GKJ8_1.ouDHjBXyzpVdVJwE9NiMR3hRHG_38uy43y6Nzk")  # TODO: get gud kid
-sdfgsdfsdfsdf
+bot.run(TOKEN)  # TODO: get gud kid
