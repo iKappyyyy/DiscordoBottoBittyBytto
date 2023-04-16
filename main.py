8bd09b91f46437a1971d3fd86532e78d73141c71
iKappyyyy <specialtimewithdaniel@gmail.com>
2023-04-16 23:12:22 +0300

    bruh

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot("")
+bot = commands.Bot(command_prefix="!!", intents=discord.Intents.all())
+
 
 @bot.event
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
 
+
 @bot.command()
-async def stop(ctx):
+async def tempalato(ctx):
     ...
 
 
-bot.run("")  # TODO: add token
+bot.run("MTA5NzE5OTc2NDIyMDY3NDA2OQ.G5Q6UO.2RdvPofpDYVf6gerwGnqNSRDM6aAGcMEI1SwAc")  # TODO: add token
